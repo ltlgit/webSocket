f71e25cabb6843db5147e6e9828f90ca7b1e72c3
tinglin.liu_c <tinglin.liu_c@huifu.com>
2022-03-04 09:56:57 +0800

    upd

--- src/main/java/com/socket/webSocket/controller/WebSocketController.java
+++ src/main/java/com/socket/webSocket/controller/WebSocketController.java
@@ -3,8 +3,10 @@ package com.socket.webSocket.controller;
 import com.socket.webSocket.service.WebSocketServer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -15,7 +17,7 @@ import java.io.IOException;
  * @author: tinglin.liu_c
  * @create: 2021-10-28 16:04
  **/
-@RestController
+@Controller
 @RequestMapping("/open/socket")
 public class WebSocketController {
 
@@ -32,9 +34,15 @@ public class WebSocketController {
      * @throws IOException
      */
     @PostMapping(value = "/onReceive")
+    @ResponseBody
     public void onReceive(String id,String pwd) throws IOException {
         if(pwd.equals(myPwd)){  //密码校验一致（这里举例，实际开发还要有个密码加密的校验的），则进行群发
             webSocketServer.broadCastInfo(id);
         }
+    }
+
+    @RequestMapping(value = "/webSocker")
+    public String webSocker() throws IOException {
+        return "webSocker";
     }
 }
